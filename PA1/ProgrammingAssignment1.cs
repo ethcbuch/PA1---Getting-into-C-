ed10e056fe8af04da23b3b3a66027d57fb9ed905
Ethan Buchanan <ethcbuch@ut.utm.edu>
2023-01-23 13:11:05 -0600

    test

--- PA1/ProgrammingAssignment1.cs
+++ PA1/ProgrammingAssignment1.cs
@@ -10,40 +10,53 @@ namespace PA1
     {
         abstract class Animal
         {
-            private string Name;
-            private int Age;
-            private string Noise;
+            private string animalName;
+            private int animalAge;
+            private string animalNoise;
+            private int animalWeight;
 
             public abstract void printInfo();
             public abstract void makeNoise();
             public abstract void ageUp();
 
+            public void ageIncrement()
+            {
+                animalAge++;
+            }
             public string getName()
             {
-                return Name;
+                return animalName;
             }
 
             public int getAge()
             {
-                return Age;
+                return animalAge;
             }
             public string getNoise()
             {
-                return Noise;
+                return animalNoise;
             }
 
             public void setName()
             {
-                Name = Console.ReadLine();
+                animalName = Console.ReadLine();
             }
             public void setNoise()
             {
-                Noise = Console.ReadLine();
+                animalNoise = Console.ReadLine();
             }
 
             public void setAge()
             {
-                string a = Console.ReadLine();
+                int a = Console.ReadLine();
+                animalAge = Convert.ToInt32(a);
+
+            }
+
+            public void setWeight()
+            {
+                int b = Console.ReadLine();
+                animalWeight = Convert.ToInt32(b);
             }
         }
 
@@ -58,31 +71,50 @@ namespace PA1
             }
             public override void makeNoise()
             {
-
+                Console.WriteLine(getNoise());
             }
 
             public override void ageUp()
             {
+                ageIncrement();
             }
         }
 
         class Cassowary : Animal
         {
+
             public override void printInfo()
             {
-
+                Console.WriteLine(getAge());
+                Console.WriteLine(getNoise());
+                Console.WriteLine(getName());
             }
             public override void makeNoise()
             {
-
+                Console.WriteLine(getNoise());
             }
 
             public override void ageUp()
             {
+                ageIncrement();
             }
         }
         static void Main(string[] args)
         {
+            Animal bigWorld = new Animal();
+            List<bigWorld> zoo = new List<bigWorld>();
+
+            Cat newCat = new Cat()
+            zoo.Add(newCat);
+            newCat.setAge();
+            newCat.setName();
+            newCat.setNoise();
+            newCat.setWeight();
+
+            newCat.printInfo();
+
+            Console.WriteLine(zoo[0]);
+            Console.ReadKey();
         }
     }
 }
